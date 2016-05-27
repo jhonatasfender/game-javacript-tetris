3409ecd9e83fea169b09c8a14f03ba24602969d8
Jonatas.Rodrigues <Jonatas.Rodrigues@Jonatas-PC.micromed.com>
2016-05-27 16:13:08 -0300

    tetris

--- stats.js
+++ stats.js
@@ -4,6 +4,6 @@ var stats = function (){
         var f,c,e;
         for (c=0;c<30;c++)
             for(f=0;f<73;f++)
-                e=(f+c*74)
+                e=(f+c*74)*4,a[e]=a[e+4],
     }
 };
