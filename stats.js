5cedde8e0ef5a18b59628ea1e7087f119dd9c164
Jonatas <Jonatas.Rodrigues@micromed.com>
2016-05-27 16:10:35 -0300

    tetris

---
+++ stats.js
@@ -0,0 +1,9 @@
+// stats.js r6 - http://github.com/mrdoob/stats.js
+var stats = function (){
+    function s(a,g,d){
+        var f,c,e;
+        for (c=0;c<30;c++)
+            for(f=0;f<73;f++)
+                e=(f+c*74)
+    }
+};
